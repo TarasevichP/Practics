dea336279f8c5ed8d7af39c08d97976d6269ee30
TarasevichP <taraspasha@gmail.com>
2020-11-05 17:46:56 +0300

    Start task jd2 C

--- src/Tarasevich/Tasks/jd01_02/TaskC.java
+++ src/Tarasevich/Tasks/jd01_02/TaskC.java
@@ -1,4 +1,32 @@
 package Tarasevich.Tasks.jd01_02;
 
+import java.util.Scanner;
+
 public class TaskC {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+
+        step1(n);
+    }
+
+
+    static void step1(int n) {
+        int[][] arr = new int[n][n];
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                arr[i][j] = (int) (n - Math.random() * n - n);
+                if (i == arr.length - 1) {
+                    System.out.println();
+                } else
+                    System.out.print(arr[i][j] + " ");
+                if (j == arr.length - 1 || i == arr.length - 1) {
+                    System.out.println();
+                }
+
+
+            }
+        }
+
+    }
 }
